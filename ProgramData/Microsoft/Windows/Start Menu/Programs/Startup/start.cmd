76fcfc1199dcd4b3e1425cbc71070b068d9b7b41
NyneDZN <nyneszn@gmail.com>
2023-04-11 12:07:17 -0700

    📂 Move start.cmd to Startup folder to initialize script

--- ProgramData/Microsoft/Windows/Start Menu/Programs/Startup/start.cmd
+++ ProgramData/Microsoft/Windows/Start Menu/Programs/Startup/start.cmd
@@ -1,24 +1,6 @@
 @echo off
-SETLOCAL EnableDelayedExpansion
-echo DO NOT CLOSE THIS WINDOW. PLEASE WAIT UNTIL YOUR PC RESTARTS.
+:: Start the post script
+start "" "%WinDir%\NeptuneDir\Tools\NSudoLG.exe" -U:T -P:E "%WinDir%\NeptuneDir\neptune-master.cmd" /postinstall
 
-:start
-set success=
-%WinDir%\NeptuneDir\Tools\NSudoLG.exe -U:T -P:E -Wait %WinDir%\NeptuneDir\neptune-master.cmd /postinstall
-
-:: read from success file
-set /p success= < C:\Users\Public\success.txt
-
-:: check if script is finished
-if %success% equ true goto success
-
-:: if not, restart script
-echo POST INSTALL SCRIPT CLOSED^^!
-echo Relaunching...
-goto start
-
-:success
-del /f /q "C:\Users\Public\success.txt"
-shutdown /r /f /t 10 /c "POST-INSTALL: Reboot is required..."
-DEL "%~f0"
-exit
+:: Delete this batch file & exit
+del /F /Q %0
--- start.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-:: Start the post script
-start "" "%WinDir%\NeptuneDir\Tools\NSudoLG.exe" -U:T -P:E "%WinDir%\NeptuneDir\neptune-master.cmd" /postinstall
-
-:: Delete this batch file & exit
-del /F /Q %0
