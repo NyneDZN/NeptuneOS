5f7ff3f60aa8dcfe0be8e3d7162cd75c54fdc548
NyneDZN <nyneszn@gmail.com>
2023-04-10 20:26:06 -0700

    ⚙️ Fix Webcam configuration scripts

--- Desktop/POST-INSTALL/3. Configuration/Webcam/Enable Webcam (Default).bat
+++ Desktop/POST-INSTALL/3. Configuration/Webcam/Enable Webcam (Default).bat
@@ -1,6 +1,7 @@
 @echo off
 Reg.exe add "HKLM\SYSTEM\CurrentControlSet\services\swenum" /v "Start" /t REG_DWORD /d "3" /f
 DevManView /enable "Plug and Play Software Device Enumerator"
+DevManView /enable "USB Video Device"
 Reg.exe add "HKLM\System\CurrentControlSet\Control\Class\{4D36E96C-E325-11CE-BFC1-08002BE10318}" /v "UpperFilters" /t REG_MULTI_SZ /d "ksthunk" /f
 Reg.exe add "HKLM\System\CurrentControlSet\Control\Class\{6BDD1FC6-810F-11D0-BEC7-08002BE2092F}" /v "UpperFilters" /t REG_MULTI_SZ /d "ksthunk" /f
 Reg.exe add "HKLM\System\CurrentControlSet\services\ksthunk" /v "Start" /t REG_DWORD /d "3" /f
