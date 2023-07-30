28732ec7d3d1569f59bf76f06af9dfffbc1c591f
NyneDZN <nyneszn@gmail.com>
2023-07-30 15:27:41 -0700

    📁 Add Start Menu configuration scripts

---
+++ Desktop/POST-INSTALL/3. Configuration/Start Menu/Enable OpenShell.cmd
@@ -0,0 +1,7 @@
+echo Enabling Open-Shell...
+ren "C:\Program Files\OpenShellOld" Open-Shell >nul 2>&1
+start "C:\Program Files\Open-Shell\StartMenu.exe" >nul 2>&1
+taskkill /f /im StartMenuExperienceHost.exe >nul 2>&1
+ren "C:\Windows\SystemApps\Microsoft.Windows.StartMenuExperienceHost_cw5n1h2txyewy\StartMenuExperienceHost.exe" ShitStartMenu.exe >nul 2>&1
+
+pause>nul
