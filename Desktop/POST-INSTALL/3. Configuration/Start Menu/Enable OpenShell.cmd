2f486a14150f2e108324f6648f1f5af036001558
NyneDZN <nyneszn@gmail.com>
2023-07-30 15:37:29 -0700

    🛠️ Fix typos in open shell script

--- Desktop/POST-INSTALL/3. Configuration/Start Menu/Enable OpenShell.cmd
+++ Desktop/POST-INSTALL/3. Configuration/Start Menu/Enable OpenShell.cmd
@@ -1,4 +1,4 @@
-echo Enabling Open-Shell...
+echo Enabling Open-Shell and disabling the Windows Start Menu
 ren "C:\Program Files\OpenShellOld" Open-Shell >nul 2>&1
 start "C:\Program Files\Open-Shell\StartMenu.exe" >nul 2>&1
 taskkill /f /im StartMenuExperienceHost.exe >nul 2>&1
