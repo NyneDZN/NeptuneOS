fee7bc339976c7e8c8977f99a14eba30419e0a88
NyneDZN <nyneszn@gmail.com>
2023-08-05 23:17:55 -0700

    ⚙️ Add TPM configuration scripts

--- Desktop/POST-INSTALL/3. Configuration/Wi-Fi/Disable Wi-Fi.bat
+++ Desktop/POST-INSTALL/3. Configuration/Wi-Fi/Disable Wi-Fi.bat
@@ -1,15 +1,6 @@
 @echo off
-echo Script credit given to Atlas.
-timeout /t 1>nul
 sc config WlanSvc start=disabled
 sc config vwififlt start=disabled
-set /P c="Would you like to disable the Network Icon? (disables 2 extra services) [Y/N]: "
-if /I "%c%" EQU "N" goto wifiDskip
-sc config netprofm start=disabled
-sc config NlaSvc start=disabled
-echo Wi-Fi and extra services have been disabled.
-pause>nul
-
-:wifiDskip
-echo Wi-Fi is now disabled, you may have to restart your PC for changes to apply.
-pause>nul
+cls
+echo WiFi disabled. Please reboot.
+pause
