9a29537d6108e603157ef7d4e5d2d3cd0c8eeb8a
NyneDZN <nyneszn@gmail.com>
2023-08-05 23:19:35 -0700

    ⚙️⚒️ Fix VPN configuration script

--- Desktop/POST-INSTALL/3. Configuration/VPN/Disable VPN Support.cmd
+++ Desktop/POST-INSTALL/3. Configuration/VPN/Disable VPN Support.cmd
@@ -1,14 +1,14 @@
 @echo off
-%WinDir%\NeptuneDir\Tools\DevManView.exe /disable "WAN Miniport (IKEv2)" >nul 2>&1 
-%WinDir%\NeptuneDir\Tools\DevManView.exe /disable "WAN Miniport (IP)" >nul 2>&1 
-%WinDir%\NeptuneDir\Tools\DevManView.exe /disable "WAN Miniport (IPv6)" >nul 2>&1 
-%WinDir%\NeptuneDir\Tools\DevManView.exe /disable "WAN Miniport (L2TP)" >nul 2>&1 
-%WinDir%\NeptuneDir\Tools\DevManView.exe /disable "WAN Miniport (Network Monitor)" >nul 2>&1 
-%WinDir%\NeptuneDir\Tools\DevManView.exe /disable "WAN Miniport (PPPOE)" >nul 2>&1 
-%WinDir%\NeptuneDir\Tools\DevManView.exe /disable "WAN Miniport (PPTP)" >nul 2>&1 
-%WinDir%\NeptuneDir\Tools\DevManView.exe /disable "WAN Miniport (SSTP)" >nul 2>&1 
-%WinDir%\NeptuneDir\Tools\DevManView.exe /disable "NDIS Virtual Network Adapter Enumerator" >nul 2>&1 
-%WinDir%\NeptuneDir\Tools\DevManView.exe /disable "Microsoft RRAS Root Enumerator" >nul 2>&1 
+%WinDir%\NeptuneDir\Tools\dmv.exe /disable "WAN Miniport (IKEv2)" >nul 2>&1 
+%WinDir%\NeptuneDir\Tools\dmv.exe /disable "WAN Miniport (IP)" >nul 2>&1 
+%WinDir%\NeptuneDir\Tools\dmv.exe /disable "WAN Miniport (IPv6)" >nul 2>&1 
+%WinDir%\NeptuneDir\Tools\dmv.exe /disable "WAN Miniport (L2TP)" >nul 2>&1 
+%WinDir%\NeptuneDir\Tools\dmv.exe /disable "WAN Miniport (Network Monitor)" >nul 2>&1 
+%WinDir%\NeptuneDir\Tools\dmv.exe /disable "WAN Miniport (PPPOE)" >nul 2>&1 
+%WinDir%\NeptuneDir\Tools\dmv.exe /disable "WAN Miniport (PPTP)" >nul 2>&1 
+%WinDir%\NeptuneDir\Tools\dmv.exe /disable "WAN Miniport (SSTP)" >nul 2>&1 
+%WinDir%\NeptuneDir\Tools\dmv.exe /disable "NDIS Virtual Network Adapter Enumerator" >nul 2>&1 
+%WinDir%\NeptuneDir\Tools\dmv.exe /disable "Microsoft RRAS Root Enumerator" >nul 2>&1 
 
 %WinDir%\NeptuneDir\Tools\PowerRun.exe /SW:0 Reg.exe add "HKLM\System\CurrentControlSet\Services\IKEEXT" /v "Start" /t REG_DWORD /d "4" /f >nul 2>&1 
 %WinDir%\NeptuneDir\Tools\PowerRun.exe /SW:0 Reg.exe add "HKLM\System\CurrentControlSet\Services\WinHttpAutoProxySv" /v "Start" /t REG_DWORD /d "4" /f >nul 2>&1 
