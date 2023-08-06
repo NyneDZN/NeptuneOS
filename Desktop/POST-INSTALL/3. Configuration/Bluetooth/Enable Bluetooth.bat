619b77db61722244fc037e204befe93870f19844
NyneDZN <nyneszn@gmail.com>
2023-08-05 23:15:52 -0700

    ⚙️⚒️ Fix bluetooth scripts

--- Desktop/POST-INSTALL/3. Configuration/Bluetooth/Enable Bluetooth.bat
+++ Desktop/POST-INSTALL/3. Configuration/Bluetooth/Enable Bluetooth.bat
@@ -1,24 +1,15 @@
 @echo off
-
-ver | findstr /i "Windows 10" > nul
-if %errorlevel% == 0 set OSVersion=Windows 10
-
-ver | findstr /i "Windows 11" > nul
-if %errorlevel% == 0 set OSVersion=Windows 11
-
-if "%OSVersion%"=="Windows 10" (
-  sc config BthAvctpSvc start=auto
-  sc stop BthAvctpSvc >nul 2>nul
-  for /f %%I in ('reg query "HKLM\SYSTEM\CurrentControlSet\Services" /s /k /f CDPUserSvc ^| find /i "CDPUserSvc" ') do (
-    reg add "%%I" /v "Start" /t REG_DWORD /d "2" /f
-    sc stop %%~nI
-    sc config CDPSvc start=auto
-  )
-)
-
-if "%OSVersion%"=="Windows 11" (
-  sc config BthAvctpSvc start=auto
-  sc start BthAvctpSvc >nul 2>nul
-)
-echo Bluetooth has been enabled.
-pause>nul
+sc config RFCOMM start=demand
+sc config BthEnum start=demand
+sc config bthleenum start=demand
+sc config BTHMODEM start=demand
+sc config BthA2dp start=demand
+sc config microsoft_bluetooth_avrcptransport start=demand
+sc config BthHFEnum start=demand
+sc config BTAGService start=demand
+sc config bthserv start=demand
+sc config BluetoothUserService start=demand
+sc config BthAvctpSvc start=demand
+cls
+echo Bluetooh enabled. Please reboot.
+pause
