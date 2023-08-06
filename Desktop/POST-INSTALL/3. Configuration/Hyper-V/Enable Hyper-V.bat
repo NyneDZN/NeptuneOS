70d8b0695ad3d21f52dd8a1af8536963cb854925
NyneDZN <nyneszn@gmail.com>
2023-08-05 23:16:50 -0700

    ⚙️ Add Hyper-V configuration script

---
+++ Desktop/POST-INSTALL/3. Configuration/Hyper-V/Enable Hyper-V.bat
@@ -0,0 +1,25 @@
+@echo off
+sc config vmickvpexchange start=demand
+sc config vmicguestinterface start=demand
+sc config vmicshutdown start=demand
+sc config vmicheartbeat start=demand
+sc config vmicvmsession start=demand
+sc config vmicrdv start=demand
+sc config vmictimesync start=demand
+sc config vmicvss start=demand
+sc config hyperkbd start=demand
+sc config hypervideo start=demand
+sc config gencounter start=demand
+sc config vmgid start=demand
+sc config storflt start=boot
+sc config bttflt start=boot
+sc config vpci start=boot
+sc config hvservice start=demand
+sc config hvcrash start=disabled
+sc config HvHost start=demand
+sc config rdbss start=system
+C:\Windows\NeptuneDir\Tools\DevManView.exe /enable "Remote Desktop Device Redirector Bus"
+C:\Windows\NeptuneDir\Tools\DevManView.exe /enable "Microsoft Hyper-V Virtualization Infrastructure Driver"
+cls
+echo Hyper-V enabled. Please reboot.
+pause
