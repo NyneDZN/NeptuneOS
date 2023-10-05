7e90c2ee7e0a762b877edd2e2ee7253a669e9321
NyneDZN <nyneszn@gmail.com>
2023-10-04 18:32:12 -0700

    fix: task manager scripts

--- Desktop/POST-INSTALL/3. Configuration/Task Manager/Revert Default Task Manager.bat
+++ Desktop/POST-INSTALL/3. Configuration/Task Manager/Revert Default Task Manager.bat
@@ -1,5 +1,5 @@
 @echo off
 %windir%\NeptuneDir\Tools\PowerRun.exe /SW:0 "Reg.exe" add "HKLM\System\CurrentControlSet\services\pcw" /v "Start" /t REG_DWORD /d "0" /f >nul 2>&1
-Reg.exe delete "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\taskmgr.exe" /v "Debugger" /t REG_SZ /d "C:\Windows\NeptuneDir\Apps\proexp.exe" /f >nul 2>&1
+Reg.exe delete "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\taskmgr.exe" /v "Debugger" /f >nul 2>&1
 echo Reverted Task Manager, you may have to restart your PC.
 pause>nul
