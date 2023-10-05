e518efd36fe175ef934418c9187005157b185a77
NyneDZN <nyneszn@gmail.com>
2023-10-04 18:31:37 -0700

    fix: TPM scripts

--- Desktop/POST-INSTALL/3. Configuration/TPM/Enable TPM [Default].bat
+++ Desktop/POST-INSTALL/3. Configuration/TPM/Enable TPM [Default].bat
@@ -1,8 +1,8 @@
 @echo off
 powershell Enable-TpmAutoProvisioning
 sc config tpm start=demand
-C:\Windows\NeptuneDir\Tools\DevManView.exe /enable "AMD PSP 10.0 Device"
-C:\Windows\NeptuneDir\Tools\DevManView.exe /enable "Trusted Platform Module 2.0"
+C:\Windows\NeptuneDir\Tools\dmv.exe /enable "AMD PSP 10.0 Device"
+C:\Windows\NeptuneDir\Tools\dmv.exe /enable "Trusted Platform Module 2.0"
 cls
 echo TPM enabled. Please reboot.
 pause>nul
