b9b14b0b96d662def13725b9e185ad0e0f1dd9da
NyneSZN <nyneszn@gmail.com>
2023-11-01 22:15:41 -0700

    rename configuration folders

---
+++ Desktop/POST-INSTALL/3. Configuration/User Account Control (UAC)/Enable UAC.cmd
@@ -0,0 +1,7 @@
+@echo off
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v "EnableLUA" /t REG_DWORD /d "1" /f > nul 2>&1
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v "PromptOnSecureDesktop" /t REG_DWORD /d "1" /f > nul 2>&1
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v "ConsentPromptBehaviorAdmin" /t REG_DWORD /d "5" /f > nul 2>&1
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\GpuEnergyDrv" /v "luafv" /t REG_DWORD /d "2" /f > nul 2>&1
+echo UAC has been enabled.
+pause>nul
