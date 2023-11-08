6b4f318a7a0bc626341413bb1fca7d5147b8e017
NyneDZN <nyneszn@gmail.com>
2023-11-08 11:09:50 -0800

    fix: UAC configuration scripts

--- Desktop/POST-INSTALL/3. Configuration/User Account Control (UAC)/Disable UAC.cmd
+++ Desktop/POST-INSTALL/3. Configuration/User Account Control (UAC)/Disable UAC.cmd
@@ -2,6 +2,6 @@
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v "EnableLUA" /t REG_DWORD /d "0" /f > nul 2>&1
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v "PromptOnSecureDesktop" /t REG_DWORD /d "0" /f > nul 2>&1
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v "ConsentPromptBehaviorAdmin" /t REG_DWORD /d "0" /f > nul 2>&1
-reg add "HKLM\SYSTEM\CurrentControlSet\Services\GpuEnergyDrv" /v "luafv" /t REG_DWORD /d "4" /f > nul 2>&1
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\luafv" /v "Start" /t REG_DWORD /d "4" /f > nul 2>&1
 echo UAC has been disabled.
 pause>nul
