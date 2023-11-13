edf62525fc1d732e33375a66bf7d0688dcb1fcd0
NyneDZN <nyneszn@gmail.com>
2023-11-13 01:37:05 -0800

    feat: add action center config scripts

---
+++ Desktop/POST-INSTALL/Optional/Windows 10/Action Center/Enable Action Center.bat
@@ -0,0 +1,3 @@
+@echo off
+Reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Explorer" /v "DisableNotificationCenter" /t REG_DWORD /d "0" /f >nul 2>&1
+taskkill /f /im explorer.exe & start explorer.exe
