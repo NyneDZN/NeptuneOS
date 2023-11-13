dc0c1400735ae9554a69ffe0e4287075e91863f0
NyneDZN <nyneszn@gmail.com>
2023-11-13 01:27:48 -0800

    revamp: post-install folder

--- Desktop/POST-INSTALL/2. Drivers/Network/Disable Driver Signature Enforcement (to install drivers).bat
+++
@@ -1,5 +0,0 @@
-@echo off
-bcdedit /set nointegritychecks on
-bcdedit /set loadoptions DISABLE_INTEGRITY_CHECKS
-bcdedit /set TESTSIGNING ON
-pause
--- Desktop/POST-INSTALL/2. Drivers/Network/Driver Signature Enforcement Revert.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-bcdedit /deletevalue nointegritychecks
-bcdedit /set loadoptions ENABLE_INTEGRITY_CHECKS
-bcdedit /deletevalue TESTSIGNING
-pause
