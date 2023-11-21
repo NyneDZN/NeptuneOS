7dde95ad67df157c6ae0f0b4bcc7305847ec0dfa
NyneSZN <nyneszn@gmail.com>
2023-11-20 16:20:03 -0800

    feat: add start menu configuration scripts

---
+++ Desktop/POST-INSTALL/3. Configuration/Start Menu/Disable Open-Shell.bat
@@ -0,0 +1,4 @@
+taskkill /f /im explorer.exe
+taskkill /f /im StartMenu.exe
+rename "C:\Program Files\Open-Shell" "OpenShellStart"
+start explorer.exe
---
+++ Desktop/POST-INSTALL/3. Configuration/Start Menu/Enable Open-Shell.bat
@@ -0,0 +1,6 @@
+taskkill /f /im explorer.exe
+rename "C:\Program Files\OpenShellStart" "Open-Shell"
+cd "C:\Program Files\Open-Shell"
+start StartMenu.exe
+cd %windir%
+start explorer.exe
