7f19353fdbd066c519f5333c8c10de4dfff8b5b3
NyneSZN <nyneszn@gmail.com>
2023-11-23 18:59:57 -0800

    feat 🚀: open-shell scripts request elevation

--- Desktop/POST-INSTALL/3. Configuration/Start Menu/Enable Open-Shell.bat
+++ Desktop/POST-INSTALL/3. Configuration/Start Menu/Enable Open-Shell.bat
@@ -1,3 +1,24 @@
+@echo off
+
+:: Check if script is escelated
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+if %errorlevel% neq 0 (
+    echo You are about to be prompted with the UAC. Please click yes when prompted.
+) ELSE (
+    goto admin
+)
+
+:prompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\prompt.vbs"
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\prompt.vbs"
+    "%temp%\prompt.vbs"
+    exit /B
+
+:admin
+:: Delete prompt script
+if exist "%temp%\prompt.vbs" ( del "%temp%\prompt.vbs" )
+
+:: Enable Open-Shell
 taskkill /f /im explorer.exe
 rename "C:\Program Files\OpenShellStart" "Open-Shell"
 cd "C:\Program Files\Open-Shell"
