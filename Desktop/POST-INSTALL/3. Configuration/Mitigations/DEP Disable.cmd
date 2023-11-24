f212415dba4ce15fb542751d02be2fce42cfb28c
NyneSZN <nyneszn@gmail.com>
2023-11-23 19:31:00 -0800

    fix 🛠️: request elevation when configuring DEP

--- Desktop/POST-INSTALL/3. Configuration/Mitigations/DEP Disable.cmd
+++ Desktop/POST-INSTALL/3. Configuration/Mitigations/DEP Disable.cmd
@@ -1,4 +1,23 @@
 @echo off
+:: Check if script is escelated
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+if %errorlevel% neq 0 (
+    echo You are about to be prompted with the UAC. Please click yes when prompted.
+) ELSE (
+    goto admin
+)
+
+:prompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\prompt.vbs"
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\prompt.vbs"
+    "%temp%\prompt.vbs"
+    exit /B
+
+:admin
+:: Delete prompt script
+if exist "%temp%\prompt.vbs" ( del "%temp%\prompt.vbs" )
+
+:: Disable DEP
 bcdedit /set nx AlwaysOff
 echo DEP has been disabled, you will have to restart your PC for changes to apply.
 pause>nul
