b46238776506eb6635343f8f89ae8b6d8d8be6b4
NyneDZN <120980797+NyneDZN@users.noreply.github.com>
2024-02-08 17:01:28 -0800

    remove programdata

--- ProgramData/Microsoft/Windows/Start Menu/Programs/Startup/start.cmd
+++
@@ -1,5 +0,0 @@
-:: Start the post script
-start "" "%WinDir%\NeptuneDir\Tools\NSudoLG.exe" -U:T -P:E "%WinDir%\NeptuneDir\neptune-master.cmd" /postinstall
-
-:: Delete this batch file & exit
-del /F /Q %0
