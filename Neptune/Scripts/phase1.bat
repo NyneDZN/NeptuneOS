c0509729494d83dd83ea139c0ddd8ba6a79ffa19
NyneDZN <nyneszn@gmail.com>
2024-02-20 15:03:41 -0800

    cmd to bat fix

---
+++ Neptune/Scripts/phase1.bat
@@ -0,0 +1,10 @@
+:: UAC Permissions
+Reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v "ConsentPromptBehaviorAdmin" /t REG_DWORD /d "0" /f > nul
+Reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v "EnableLUA" /t REG_DWORD /d "0" /f > nul
+Reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v "PromptOnSecureDesktop" /t REG_DWORD /d "0" /f > nul
+
+::: Finalize
+echo Restarting...
+Reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce" /v "Neptune" /t REG_SZ /d "C:\Neptune\nsudo.exe -U:T -P:E -ShowWindowMode:Show -Wait \"C:\Neptune\Scripts\phase2.cmd\"" /f > nul
+timeout /t 3 > nul
+shutdown /f /r /t 0
---
+++ Neptune/neptune.bat
@@ -0,0 +1,25 @@
+@echo off
+:: Check if script is escelated
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+if %errorlevel% neq 0 (
+    echo You are about to be prompted with the UAC. Please click yes when prompted.
+) ELSE (
+    goto admin
+)
+
+:prompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\prompt.vbs"
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\prompt.vbs"
+    "%temp%\prompt.vbs"
+    exit /B
+
+:admin
+:: Delete prompt script
+if exist "%temp%\prompt.vbs" ( del "%temp%\prompt.vbs" )
+
+
+echo NeptuneOS Installer
+echo Welcome to the NeptuneOS Installer. Please report any issues you encounter with the script in the Discord or GitHub.
+echo Press any key to let the script restart your PC and install NeptuneOS.
+pause>nul
+call C:\Neptune\Scripts\phase1.cmd
