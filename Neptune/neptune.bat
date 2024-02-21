565d92e1bdd7cc9b4ce11f2b66955ee9699a55a9
NyneDZN <nyneszn@gmail.com>
2024-02-21 14:15:11 -0800

    delete installer.zip

--- Neptune/neptune.bat
+++ Neptune/neptune.bat
@@ -21,6 +21,7 @@ if exist "%temp%\prompt.vbs" ( del "%temp%\prompt.vbs" )
 echo NeptuneOS Installer
 echo Welcome to the NeptuneOS Installer. Please report any issues you encounter with the script in the Discord or GitHub.
 echo Press any key to let the script restart your PC and install NeptuneOS.
+del "%temp%\installer.zip"
 taskkill /f /im powershell.exe >nul
 pause>nul
 call C:\Neptune\Scripts\phase1.bat
