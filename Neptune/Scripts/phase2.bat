5bf4b5678595e76867c38485b3f4d885602113bf
NyneDZN <nyneszn@gmail.com>
2024-02-21 15:33:41 -0800

    update caption & timeout instead of pause

--- Neptune/Scripts/phase2.bat
+++ Neptune/Scripts/phase2.bat
@@ -17,10 +17,9 @@ rmdir /s /q "C:\Windows\Web" >nul
 move "C:\NeptuneOS-installer\Neptune\NeptuneOS-main\Web" "%WinDir%" >nul
 cls
 echo This will execute 2 command prompt windows for the installation process. Please ignore any errors you might see in both windows, as these are normal.
-echo The first window will finish early, and the second one will continue for a moment before asking you to restart.
-echo All of this code is open-source and available to you in these locations. "C:\Neptune\Scripts" and "C:\Windows\NeptuneDir"
-echo Press any key to start the script.
-pause>nul
+echo Wait until you are asked to restart your PC.
+echo The script will start in 10 seconds, or you can press any key.
+timeout /t 10 >nul
 :: The Master Script should always be infront of the Package Debloat script window.
 start "First Batch" cmd /c call "%WinDir%\NeptuneDir\packages.bat"
 start "Second Batch" "%WinDir%\NeptuneDir\Tools\NSudoLG.exe" -U:T -P:E "%WinDir%\NeptuneDir\neptune-master.bat" /postinstall
