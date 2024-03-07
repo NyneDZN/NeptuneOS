650e814d335b764e266f068b8dcaba40fd2649c2
NyneDZN <nyneszn@gmail.com>
2024-03-06 18:58:14 -0800

    update(post-folder): MSMB script refactor

--- NeptuneDir/POST-INSTALL/4. Advanced Configuration/Services and Drivers/Microsoft System Management BIOS/Disable MSMB.bat
+++ NeptuneDir/POST-INSTALL/4. Advanced Configuration/Services and Drivers/Microsoft System Management BIOS/Disable MSMB.bat
@@ -1,7 +1,8 @@
 @echo off
-echo Warning: Disabling the Microsoft System Management BIOS Driver will break functionality for the system and programs, such as GTAV.
-echo Close the script if you do not want to continue.
+echo Warning: Disabling the Microsoft System Management BIOS Driver will disable functionality for some applications, such as GTA5 and FiveM.
+echo Please close this script if you do not want to continue.
 pause>nul
 %windir%\neptunedir\tools\dmv.exe /disable "Microsoft System Management BIOS Driver"
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\mssmbios" /v "Start" /t REG_DWORD /d "4" /f > nul 2>&1
 echo The Microsoft System Management BIOS Driver has been disabled.
+timeout /t 2 >nul
