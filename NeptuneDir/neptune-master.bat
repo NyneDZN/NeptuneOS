b400b41ea3c71c64b6ed42bbe5b5313b3d94d7a4
NyneDZN <nyneszn@gmail.com>
2024-03-13 12:31:36 -0700

    fix(script): typos

--- NeptuneDir/neptune-master.bat
+++ NeptuneDir/neptune-master.bat
@@ -2248,9 +2248,9 @@ if "%os%"=="Windows 10" (
     icacls "C:\Windows\SystemApps\Microsoft.Windows.Search_cw5n1h2txyewy\searchapp.exe" /grant Administrators:F >nul 2>&1
     ren searchapp.exe searchapp.old >nul 2>&1
     cd C:\Windows\SystemApps\MicrosoftWindows.Client.CBS_cw5n1h2txyewy >nul 2>&1
+    icacls "C:\Windows\SystemApps\MicrosoftWindows.Client.CBS_cw5n1h2txyewy\SearchHost.exe" /grant Administrators:F >nul 2>&1
     takeown /f "SearchHost.exe" >nul 2>&1
     taskkill /f /im TextInputHost.exe
-    icacls "C:\Windows\SystemApps\MicrosoftWindows.Client.CBS_cw5n1h2txyewy\SearchHost.exe" /grant Administrators:F >nul 2>&1
     takeown /f "TextInputHost.exe" >nul 2>&1
     icacls "C:\Windows\SystemApps\MicrosoftWindows.Client.CBS_cw5n1h2txyewy\TextInputHost.exe" /grant Administrators:F >nul 2>&1
     ren TextInputHost.exe TextInputHost.old >nul 2>&1
