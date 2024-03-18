1b7afe41a83aae9a33663ab37dc85adc1c156720
NyneDZN <nyneszn@gmail.com>
2024-03-18 00:47:22 -0700

    feat(post-folder): volmgrx driver configuration

---
+++ NeptuneDir/POST-INSTALL/4. Advanced Configuration/Services, Drivers/volmgrx.bat
@@ -0,0 +1,37 @@
+@echo off
+
+:: Check if script is escelated
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+if %errorlevel% neq 0 (
+    echo You are about to be prompted with the UAC. Please click yes when prompted.
+) ELSE (
+    goto admin
+)
+
+:prompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\prompt.vbs"
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\prompt.vbs"
+    "%temp%\prompt.vbs"
+    exit /B
+
+:admin
+:: Delete prompt script
+if exist "%temp%\prompt.vbs" ( del "%temp%\prompt.vbs" )
+
+
+echo.  Disabling the volmgrx driver will break dynamic volumes and RAID.
+echo.  Use at your own risk
+
+
+echo.	Press 1 to Enable
+echo.	Press 2 to Disable
+echo.
+set /p c="Enter your answer: "
+if /i %c% equ 1 goto :enable
+if /i %c% equ 2 goto :disable
+
+:disable
+Reg add "HKLM\SYSTEM\CurrentControlSet\Services\volmgrx" /v "Start" /t REG_DWORD /d "4" /f > nul 
+
+:enable
+Reg add "HKLM\SYSTEM\CurrentControlSet\Services\volmgrx" /v "Start" /t REG_DWORD /d "0" /f > nul 
