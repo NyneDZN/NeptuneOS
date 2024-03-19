113dd6937d7c05e48b0958d539f7177fea75eff9
NyneDZN <nyneszn@gmail.com>
2024-03-19 11:51:04 -0700

    fix(script): onedrive uninstallation

--- NeptuneDir/neptune-master.bat
+++ NeptuneDir/neptune-master.bat
@@ -2184,8 +2184,8 @@ Microsoft.MSPaint
 :: Remove OneDrive
 taskkill /f /im OneDrive.exe
 taskkill /f /im OneDrive.exe
-if "%os%"=="Windows 11" (%currentuser% start /wait "" "%SYSTEMROOT%\System32\ONEDRIVESETUP.EXE" /UNINSTALL >nul) 
-if "%os%"=="Windows 10" (%currentuser% start /wait "" "%SYSTEMROOT%\SysWOW64\ONEDRIVESETUP.EXE" /UNINSTALL >nul)
+if exist "%SYSTEMROOT%\System32\OneDriveSetup.exe" ("%SYSTEMROOT%\System32\OneDriveSetup.exe" /uninstall) else ("%SYSTEMROOT%\SysWOW64\OneDriveSetup.exe" /uninstall) > nul
+    
 
 :: Remove OneDrive Startup Task
 %currentuser% Reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "OneDrive" /f >nul 2>&1
