7b4fd7bcb88aabf4a916584b266b0448bc02aab6
NyneDZN <nyneszn@gmail.com>
2024-03-20 21:36:22 -0700

    don't readd regkey

--- NeptuneDir/finalize.bat
+++ NeptuneDir/finalize.bat
@@ -1,10 +1,9 @@
 @echo off
 echo Doing some cleanup before final use, this won't take long.
-Reg.exe delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v "legalnoticecaption" /f >nul
-Reg.exe delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v "legalnoticetext" /f >nul
-Reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /f >nul
-Reg.exe delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce" /f >nul
-Reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce" /f >nul
+Reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v "legalnoticecaption" /t REG_SZ /d "" /f >nul
+Reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v "legalnoticetext" /t REG_SZ /d "" /f >nul
+Reg delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce" /f >nul
+Reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce" /f >nul
 del /s /f /q C:\Windows\Temp\*.* >nul 2>&1
 del /s /f /q %USERPROFILE%\appdata\local\temp\*.* >nul 2>&1
 cd %systemroot% & del *.log /s /f /q /a >nul 2>&1
