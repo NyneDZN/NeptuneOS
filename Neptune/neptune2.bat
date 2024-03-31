6b8b650979cd39997ed34af059187bd6a73ca32c
NyneDZN <nyneszn@gmail.com>
2024-03-31 13:49:39 -0700

    use WebClient over curl and tidy up

--- Neptune/neptune2.bat
+++ Neptune/neptune2.bat
@@ -1,29 +1,24 @@
 @echo off && color f1
-mode 40,20
+mode con: cols=40 lines=20
 echo Downloading NeptuneOS...
-curl -o neptune.7z -L -s https://github.com/NyneDZN/NeptuneOS/archive/refs/heads/main.zip
-cls
-echo Initializing Installer...
-move neptune.7z C:\NeptuneOS-installer-dev\Neptune >nul
-cd "C:\NeptuneOS-installer-dev\Neptune" >nul
+"%WinDir%\System32\WindowsPowerShell\v1.0\powershell.exe" -Command "(New-Object System.Net.WebClient).DownloadFile('https://github.com/NyneDZN/NeptuneOS/archive/refs/heads/main.zip', 'C:\NeptuneOS-installer-dev\Neptune\neptune.7z');"
+cls & echo Initializing Installer...
+:: Extract and Delete .7z
 7za x neptune.7z >nul
 del neptune.7z >nul
+:: Change Directory into repository folder
 cd "NeptuneOS-main" >nul
+:: Move Neptune Modules to WinDir
 move NeptuneDir %WinDir% >nul
+:: Move Desktop Shortcut to USERS Desktop
+move "C:\NeptuneOS-installer-dev\Neptune\NeptuneOS-main\Desktop\Neptune.lnk" "C:\Users\%USERNAME%\Desktop" > nul
+:: Move Neptune Utilities to WinDir
 for %%a in (layout.xml, regjump.exe, serviwin.exe) do (move "%%a" "%WinDir%")
-:: move "C:\NeptuneOS-installer-dev\Neptune\NeptuneOS-main\layout.xml" "%WinDir%" >nul
-:: move "C:\NeptuneOS-installer-dev\Neptune\NeptuneOS-main\regjump.exe" "%WinDir%" >nul
-:: move "C:\NeptuneOS-installer-dev\Neptune\NeptuneOS-main\serviwin.exe" "%WinDir%" >nul
-move "C:\NeptuneOS-installer-dev\Neptune\NeptuneOS-main\Desktop\Neptune.lnk" "C:\Users\%USERNAME%\Desktop"
-takeown /f "C:\ProgramData\Microsoft\User Account Pictures" /r && icacls C:\ProgramData\Microsoft\User Account Pictures\ /grant administrators:F /T >nul
-rmdir /s /q "C:\ProgramData\Microsoft\User Account Pictures" >nul
-move "C:\NeptuneOS-installer-dev\Neptune\NeptuneOS-main\ProgramData\Microsoft\User Account Pictures" "C:\ProgramData\Microsoft" >nul
-takeown /f "C:\Windows\Web" /r && icacls C:\Windows\Web\ /grant administrators:F /T >nul
-rmdir /s /q "C:\Windows\Web" >nul
-move "C:\NeptuneOS-installer-dev\Neptune\NeptuneOS-main\Web" "%WinDir%" >nul
+:: Move Neptune Wallpapers
+takeown /f "C:\Windows\Web" /r && icacls C:\Windows\Web\ /grant administrators:F /T && rmdir /s /q "C:\Windows\Web" && move "C:\NeptuneOS-installer-dev\Neptune\NeptuneOS-main\Web" "%WinDir%" >nul
+:: Delete the initial batch file
 del /q /f "C:\Users\%USERNAME%\Desktop\neptune_dev.bat" >nul
-cls
 :: Install Neptune
-echo Opening Installer...
+cls & echo Opening Installer...
 timeout /t 2 >nul
 start "" "C:\Windows\NeptuneDir\neptune-master.bat" /postinstall /devbuild
