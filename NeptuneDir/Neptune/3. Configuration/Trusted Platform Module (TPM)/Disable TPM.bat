65e9f72bbd3a69212344ab530c1a311b0efb75f4
NyneDZN <nyneszn@gmail.com>
2024-04-02 10:33:04 -0700

    fix(script): TPM permissions

--- NeptuneDir/Neptune/3. Configuration/Trusted Platform Module (TPM)/Disable TPM.bat
+++ NeptuneDir/Neptune/3. Configuration/Trusted Platform Module (TPM)/Disable TPM.bat
@@ -1,5 +1,5 @@
 @echo off
-powershell Disable-TpmAutoProvisioning
+PowerShell -ExecutionPolicy Unrestricted Disable-TpmAutoProvisioning
 sc config tpm start=disabled
 C:\Windows\NeptuneDir\Tools\dmv.exe /disable "AMD PSP 10.0 Device"
 C:\Windows\NeptuneDir\Tools\dmv.exe /disable "Trusted Platform Module 2.0"
