d1d804e9f727e2de5914020bdd645facf153ad77
NyneDZN <nyneszn@gmail.com>
2024-04-02 14:00:34 -0700

    refactor(post-folder): call ANSI.bat

--- NeptuneDir/Neptune/3. Configuration/Firewall/Toggle Firewall.bat
+++ NeptuneDir/Neptune/3. Configuration/Firewall/Toggle Firewall.bat
@@ -1,12 +1,7 @@
 @echo off
 setlocal EnableDelayedExpansion
 mode 60,20
-cd /d "%~dp0"
-for /f %%a in ('forfiles /m "%~nx0" /c "cmd /c echo 0x1B"') do set "ESC=%%a"
-set "right=%ESC%[<x>C"
-set "bullet= %ESC%[34m-%ESC%[0m"
-set "CMDLINE=RED=[31m,S_GRAY=[90m,S_RED=[91m,S_GREEN=[92m,S_YELLOW=[93m,S_MAGENTA=[95m,S_WHITE=[97m,B_BLACK=[40m,B_YELLOW=[43m,UNDERLINE=[4m,_UNDERLINE=[24m"
-set "%CMDLINE:,=" & set "%"
+cd %WinDir%\NeptuneDir >nul && where ansi.bat >nul && call ansi.bat >nul
 
 
 echo              !S_RED!%ESC%[7mFirewall Configuration%ESC%[0m
