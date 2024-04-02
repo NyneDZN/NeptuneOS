59f8837a4d8859f424e61f33e5c4aaa9eb4655a9
NyneDZN <nyneszn@gmail.com>
2024-04-02 12:36:10 -0700

    feat(script): add ANSI scriptwrapper

---
+++ NeptuneDir/ANSI.bat
@@ -0,0 +1,8 @@
+:: Credit AtlasOS
+:: Modified a tad by Nyne
+cd /d "%~dp0"
+for /f %%a in ('forfiles /m "%~nx0" /c "cmd /c echo 0x1B"') do set "ESC=%%a"
+set "right=%ESC%[<x>C"
+set "bullet= %ESC%[34m-%ESC%[0m"
+set "CMDLINE=RED=[31m,S_GRAY=[90m,S_RED=[91m,S_GREEN=[92m,S_YELLOW=[93m,S_MAGENTA=[95m,S_WHITE=[97m,B_BLACK=[40m,B_YELLOW=[43m,UNDERLINE=[4m,_UNDERLINE=[24m"
+set "%CMDLINE:,=" & set "%"
