8149221f1d17129f4f87241bb75f229019a4bcd1
NyneDZN <nyneszn@gmail.com>
2024-04-02 22:20:49 -0700

    fix(post-folder): choco browser scripts

--- NeptuneDir/Neptune/1. Install Software/Install Brave.bat
+++ NeptuneDir/Neptune/1. Install Software/Install Brave.bat
@@ -1,1 +1,20 @@
-choco install brave -y
+@echo off
+:: Check if script is escelated
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+if %errorlevel% neq 0 (
+    echo You are about to be prompted with the UAC. Please click yes when prompted.
+) ELSE (
+    goto admin
+)
+
+:prompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\prompt.vbs"
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\prompt.vbs"
+    "%temp%\prompt.vbs"
+    exit /B
+
+:admin
+:: Delete prompt script
+if exist "%temp%\prompt.vbs" ( del "%temp%\prompt.vbs" )
+@echo on
+choco install brave -y --ignore-checksums
