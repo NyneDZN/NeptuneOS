379a4d50329e4be76ab7545c862a10ce9b845065
NyneDZN <nyneszn@gmail.com>
2024-04-06 15:43:50 -0700

    remove(post-folder): old process explorer scripts

--- NeptuneDir/Neptune/3. Configuration/Task Manager/Revert Default Task Manager.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-%windir%\NeptuneDir\Tools\PowerRun.exe /SW:0 "Reg.exe" add "HKLM\System\CurrentControlSet\services\pcw" /v "Start" /t REG_DWORD /d "0" /f >nul 2>&1
-Reg.exe delete "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\taskmgr.exe" /v "Debugger" /f >nul 2>&1
-echo Reverted Task Manager, you may have to restart your PC.
-pause>nul
