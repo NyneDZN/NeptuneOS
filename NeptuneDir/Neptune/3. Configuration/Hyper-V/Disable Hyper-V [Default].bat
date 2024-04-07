2c16618bc8fd8bc4179090bee31d6bbd4791484a
NyneDZN <nyneszn@gmail.com>
2024-04-06 18:20:48 -0700

    fix(post-folder): hyper-v configuration

--- NeptuneDir/Neptune/3. Configuration/Hyper-V/Disable Hyper-V [Default].bat
+++ NeptuneDir/Neptune/3. Configuration/Hyper-V/Disable Hyper-V [Default].bat
@@ -1,23 +1,94 @@
 @echo off
-sc config vmickvpexchange start=disabled
-sc config vmicguestinterface start=disabled
-sc config vmicshutdown start=disabled
-sc config vmicheartbeat start=disabled
-sc config vmicvmsession start=disabled
-sc config vmicrdv start=disabled
-sc config vmictimesync start=disabled
-sc config vmicvss start=disabled
-sc config hyperkbd start=disabled
-sc config hypervideo start=disabled
-sc config gencounter start=disabled
-sc config vmgid start=disabled
-sc config storflt start=disabled
-sc config bttflt start=disabled
-sc config vpci start=disabled
-sc config hvservice start=disabled
-sc config hvcrash start=disabled
-sc config HvHost start=disabled
-C:\Windows\NeptuneDir\Tools\dmv.exe /disable "Remote Desktop Device Redirector Bus"
+setlocal EnableDelayedExpansion
+cd %WinDir%\NeptuneDir\Scripts >nul && where ansi.cmd >nul && call ansi.cmd >nul
+
+:: Call Administrator
+fltmc >nul 2>&1 || (
+    echo Administrator privileges are required.
+    PowerShell -NoProfile Start -Verb RunAs '%0' 2> nul || (
+        echo Right-click on the script and select 'Run as administrator'.
+        pause & exit 1
+    )
+    exit 0
+)
+
+echo !S_YELLOW!This will disable the Hyper-V component.
+choice /C YN /M "Are you sure you want to continue?"
+if errorlevel 2 (
+    goto Nope
+) else (
+    goto DisableHyperV
+)
+
+
+:DisableHyperV
+cls & echo !S_YELLOW!Disabling Hyper-V
+:: Hyper-V Services/Drivers
+sc config bttflt start=disabled > nul
+sc config gcs start=disabled > nul
+sc config gencounter start=disabled > nul
+sc config hvcrash start=disabled > nul
+sc config hvhost start=disabled > nul
+sc config hvservice start=disabled > nul
+sc config hvsocketcontrol start=disabled > nul
+sc config passthruparser start=disabled > nul
+sc config pvhdparser start=disabled > nul
+sc config spaceparser start=disabled > nul
+sc config storflt start=disabled > nul
+sc config vhdparser start=disabled > nul
+sc config Vid start=disabled > nul
+sc config vkrnlintvsc start=disabled > nul
+sc config vkrnlintvsp start=disabled > nul
+sc config vmbus start=disabled > nul
+sc config vmbusr start=disabled > nul
+sc config vmcompute start=disabled > nul
+sc config vmgid start=disabled > nul
+sc config vmicguestinterface start=disabled > nul
+sc config vmicheartbeat start=disabled > nul
+sc config vmickvpexchange start=disabled > nul
+sc config vmicrdv start=disabled > nul
+sc config vmicshutdown start=disabled > nul
+sc config vmictimesync start=disabled > nul
+sc config vmicvmsession start=disabled > nul
+sc config vmicvss start=disabled > nul
+sc config vpci start=disabled > nul
+
+:: Hyper-V Devices
+C:\Windows\NeptuneDir\Tools\dmv.exe /disable "Microsoft Hyper-V NT Kernel Integration VSP"
+C:\Windows\NeptuneDir\Tools\dmv.exe /disable "Microsoft Hyper-V PCI Server"
+C:\Windows\NeptuneDir\Tools\dmv.exe /disable "Microsoft Hyper-V Virtual Disk Server"
+C:\Windows\NeptuneDir\Tools\dmv.exe /disable "Microsoft Hyper-V Virtual Machine Bus Provider"
+C:\Windows\NeptuneDir\Tools\dmv.exe /disable "Microsoft Hyper-V Virtualization Infrastructure Driver"
+
+:: DISM
+dism /online /disable-feature:Microsoft-Hyper-V-All /quiet /norestart
+
+:: BCD
+bcdedit /set hypervisorlaunchtype off > nul
+bcdedit /set vm no > nul
+bcdedit /set vmslaunchtype Off > nul
+bcdedit /set loadoptions DISABLE-LSA-ISO,DISABLE-VBS > nul
+
+:: REGEDIT
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\DeviceGuard" /t REG_DWORD /v "EnableVirtualizationBasedSecurity" /d "0" /f > nul
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\DeviceGuard" /t REG_DWORD /v "RequirePlatformSecurityFeatures" /d "1" /f > nul
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\DeviceGuard" /t REG_DWORD /v "HypervisorEnforcedCodeIntegrity" /d "0" /f > nul
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\DeviceGuard" /t REG_DWORD /v "HVCIMATRequired" /d "0" /f > nul
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\DeviceGuard" /t REG_DWORD /v "LsaCfgFlags" /d "0" /f > nul
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\DeviceGuard" /t REG_DWORD /v "ConfigureSystemGuardLaunch" /d "0" /f > nul
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\DeviceGuard" /v "RequireMicrosoftSignedBootChain" /t REG_DWORD /d "0" /f > nul
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\DeviceGuard\Scenarios\HypervisorEnforcedCodeIntegrity" /v "WasEnabledBy" /t REG_DWORD /d "0" /f > nul
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\DeviceGuard\Scenarios\HypervisorEnforcedCodeIntegrity" /v "Enabled" /t REG_DWORD /d "0" /f > nul
+
 cls
-echo Hyper-V disabled. Please reboot.
-pause
+:: Echo to Log
+echo Hyper-V was disabled >> %neptlog%
+:: Echo to User
+echo !S_YELLOW!Hyper-V disabled. Please reboot.
+timeout /t 3 >nul
+exit
+
+
+
+:Nope
+exit
--- NeptuneDir/Neptune/3. Configuration/Hyper-V/Enable Hyper-V.bat
+++ NeptuneDir/Neptune/3. Configuration/Hyper-V/Enable Hyper-V.bat
@@ -1,25 +1,81 @@
 @echo off
-sc config vmickvpexchange start=demand
-sc config vmicguestinterface start=demand
-sc config vmicshutdown start=demand
-sc config vmicheartbeat start=demand
-sc config vmicvmsession start=demand
-sc config vmicrdv start=demand
-sc config vmictimesync start=demand
-sc config vmicvss start=demand
-sc config hyperkbd start=demand
-sc config hypervideo start=demand
-sc config gencounter start=demand
-sc config vmgid start=demand
-sc config storflt start=boot
-sc config bttflt start=boot
-sc config vpci start=boot
-sc config hvservice start=demand
-sc config hvcrash start=disabled
-sc config HvHost start=demand
-sc config rdbss start=system
-C:\Windows\NeptuneDir\Tools\dmv.exe /enable "Remote Desktop Device Redirector Bus"
+setlocal EnableDelayedExpansion
+cd %WinDir%\NeptuneDir\Scripts >nul && where ansi.cmd >nul && call ansi.cmd >nul
+
+:: Call Administrator
+fltmc >nul 2>&1 || (
+    echo Administrator privileges are required.
+    PowerShell -NoProfile Start -Verb RunAs '%0' 2> nul || (
+        echo Right-click on the script and select 'Run as administrator'.
+        pause & exit 1
+    )
+    exit 0
+)
+
+echo !S_YELLOW!Enabling Hyper-V
+:: Hyper-V Services/Drivers
+sc config hvcrash start=disabled > nul
+sc config hvservice start=manual > nul
+sc config vhdparser start=manual > nul
+sc config vmbus start=boot > nul
+sc config Vid start=system > nul
+sc config bttflt start=boot > nul
+sc config gencounter start=manual > nul
+sc config hvsocketcontrol start=manual > nul
+sc config passthruparser start=manual > nul
+sc config pvhdparser start=manual > nul
+sc config spaceparser start=manual > nul
+sc config storflt start=boot > nul
+sc config vmgid start=manual > nul
+sc config vmbusr start=manual > nul
+sc config vpci start=boot > nul
+sc config gcs start=manual > nul
+sc config hvhost start=manual > nul
+sc config vmcompute start=manual > nul
+sc config vmicguestinterface start=manual > nul
+sc config vmicheartbeat start=manual > nul
+sc config vmickvpexchange start=manual > nul
+sc config vmicrdv start=manual > nul
+sc config vmicshutdown start=manual > nul
+sc config vmictimesync start=manual > nul
+sc config vmicvmsession start=manual > nul
+sc config vmicvss start=manual > nul
+
+:: Hyper-V Devices
+C:\Windows\NeptuneDir\Tools\dmv.exe /enable "Microsoft Hyper-V NT Kernel Integration VSP"
+C:\Windows\NeptuneDir\Tools\dmv.exe /enable "Microsoft Hyper-V PCI Server"
+C:\Windows\NeptuneDir\Tools\dmv.exe /enable "Microsoft Hyper-V Virtual Disk Server"
+C:\Windows\NeptuneDir\Tools\dmv.exe /enable "Microsoft Hyper-V Virtual Machine Bus Provider"
 C:\Windows\NeptuneDir\Tools\dmv.exe /enable "Microsoft Hyper-V Virtualization Infrastructure Driver"
+
+:: DISM
+dism /online /disable-feature:Microsoft-Hyper-V-All /quiet /norestart
+
+:: BCD
+bcdedit /set loadoptions DISABLE-LSA-ISO,DISABLE-VBS > nul
+bcdedit /deletevalue loadoptions > nul
+bcdedit /set vsmlaunchtype Auto > nul
+bcdedit /set hypervisorlaunchtype auto > nul
+bcdedit /deletevalue vm > nul
+
+:: REGEDIT
+reg delete "HKLM\SOFTWARE\Policies\Microsoft\Windows\DeviceGuard" /v "EnableVirtualizationBasedSecurity" /f > nul
+reg delete "HKLM\SOFTWARE\Policies\Microsoft\Windows\DeviceGuard" /v "RequirePlatformSecurityFeatures" /f > nul
+reg delete "HKLM\SOFTWARE\Policies\Microsoft\Windows\DeviceGuard" /v "HypervisorEnforcedCodeIntegrity" /f > nul
+reg delete "HKLM\SOFTWARE\Policies\Microsoft\Windows\DeviceGuard" /v "HVCIMATRequired" /f > nul
+reg delete "HKLM\SOFTWARE\Policies\Microsoft\Windows\DeviceGuard" /v "LsaCfgFlags" /f > nul
+reg delete "HKLM\SOFTWARE\Policies\Microsoft\Windows\DeviceGuard" /v "ConfigureSystemGuardLaunch" /f > nul
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\DeviceGuard" /v "RequireMicrosoftSignedBootChain" /t REG_DWORD /d "1" /f > nul
+reg delete "HKLM\SYSTEM\CurrentControlSet\Control\DeviceGuard\Scenarios\HypervisorEnforcedCodeIntegrity" /v "WasEnabledBy" /f > nul
+reg delete "HKLM\SYSTEM\CurrentControlSet\Control\DeviceGuard\Scenarios\HypervisorEnforcedCodeIntegrity" /v "Enabled" /f > nul
+
+
+
 cls
-echo Hyper-V enabled. Please reboot.
-pause
+:: Echo to Log
+echo Hyper-V was enabled >> %neptlog%
+:: Echo to User
+echo !S_YELLOW!Hyper-V enabled. Please reboot.
+timeout /t 3 >nul
+exit
+
