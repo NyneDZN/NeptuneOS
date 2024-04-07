0017e382c89006cd4d64d67ad4c7b846f9d06882
NyneDZN <nyneszn@gmail.com>
2024-04-06 17:35:51 -0700

    move(post-folder): MPV installer to software folder

--- NeptuneDir/Neptune/3. Configuration/Media Player/Disable Windows Media Player.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-dism /Online /Disable-Feature /FeatureName:"MediaPlayback" /NoRestart >nul 2>&1
-dism /Online /Disable-Feature /FeatureName:"WindowsMediaPlayer" /NoRestart >nul 2>&1
-echo Windows Media Player has been disabled. 
-pause>nul
--- NeptuneDir/Neptune/3. Configuration/Media Player/Enable Windows Media Player.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-dism /Online /Enable-Feature /FeatureName:"MediaPlayback" /NoRestart >nul 2>&1
-dism /Online /Enable-Feature /FeatureName:"WindowsMediaPlayer" /NoRestart >nul 2>&1
-echo Windows Media Player has been enabled. 
-pause>nul
