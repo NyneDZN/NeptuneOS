a6e8c0266d9a0205aa0b8e42413c92945a4c9fbc
NyneDZN <nyneszn@gmail.com>
2024-04-12 03:55:14 -0700

    fix(post-folder): firewall script elevation

--- NeptuneDir/Neptune/6. Security/Firewall/Toggle Firewall.bat
+++ NeptuneDir/Neptune/6. Security/Firewall/Toggle Firewall.bat
@@ -3,6 +3,17 @@ setlocal EnableDelayedExpansion
 mode 60,20
 cd %WinDir%\NeptuneDir\Scripts >nul && where ansi.cmd >nul && call ansi.cmd >nul
 
+:: Call Administrator
+fltmc >nul 2>&1 || (
+    echo Administrator privileges are required.
+    PowerShell -NoProfile Start -Verb RunAs '%0' 2> nul || (
+        echo Right-click on the script and select 'Run as administrator'.
+        pause & exit 1
+    )
+    exit 0
+)
+
+
 
 echo              !S_RED!%ESC%[7mFirewall Configuration%ESC%[0m
 echo]
