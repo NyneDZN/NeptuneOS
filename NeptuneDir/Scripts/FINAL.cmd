05b0ed0e5770570b5b0124f4c4c423da0c34a20f
NyneDZN <nyneszn@gmail.com>
2024-04-11 17:39:16 -0700

    fix(script): typo in devices

--- NeptuneDir/Scripts/FINAL.cmd
+++ NeptuneDir/Scripts/FINAL.cmd
@@ -8,32 +8,22 @@ Reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce" /f >nul
 :: Explorer Patcher
 Reg.exe add "HKCU\Software\ExplorerPatcher" /v "ImportOK" /t REG_DWORD /d "1" /f
 Reg.exe add "HKCU\Software\ExplorerPatcher" /v "OldTaskbar" /t REG_DWORD /d "0" /f
-Reg.exe add "HKCU\Software\Microsoft\Windows\CurrentVersion\Search" /v "SearchboxTaskbarMode" /t REG_DWORD /d "2" /f
-Reg.exe add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "ShowTaskViewButton" /t REG_DWORD /d "0" /f
-Reg.exe add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "TaskbarDa" /t REG_DWORD /d "1" /f
-Reg.exe add "HKCU\Software\ExplorerPatcher" /f
 Reg.exe add "HKCU\Software\ExplorerPatcher" /v "SkinMenus" /t REG_DWORD /d "1" /f
 Reg.exe add "HKCU\Software\ExplorerPatcher" /v "CenterMenus" /t REG_DWORD /d "1" /f
 Reg.exe add "HKCU\Software\ExplorerPatcher" /v "FlyoutMenus" /t REG_DWORD /d "1" /f
 Reg.exe add "HKCU\Software\Microsoft\TabletTip\1.7" /v "TipbandDesiredVisibility" /t REG_DWORD /d "0" /f
 Reg.exe add "HKCU\Software\ExplorerPatcher" /v "HideControlCenterButton" /t REG_DWORD /d "0" /f
 Reg.exe add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "TaskbarSD" /t REG_DWORD /d "1" /f
-Reg.exe add "HKCU\Software\ExplorerPatcher" /f
-Reg.exe add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /ve /t REG_SZ /d "" /f
 Reg.exe add "HKCU\Software\ExplorerPatcher" /v "LegacyFileTransferDialog" /t REG_DWORD /d "0" /f
 Reg.exe add "HKCU\Software\ExplorerPatcher" /v "UseClassicDriveGrouping" /t REG_DWORD /d "0" /f
-Reg.exe add "HKCU\Software\ExplorerPatcher" /f
 Reg.exe add "HKCU\Software\ExplorerPatcher" /v "DisableImmersiveContextMenu" /t REG_DWORD /d "1" /f
 Reg.exe delete "HKCU\Software\Classes\CLSID\{056440FD-8568-48e7-A632-72157243B55B}\InprocServer32" /f
-Reg.exe add "HKCU\Software\ExplorerPatcher" /f
 Reg.exe add "HKCU\Software\ExplorerPatcher" /v "ShrinkExplorerAddressBar" /t REG_DWORD /d "1" /f
 Reg.exe add "HKCU\Software\ExplorerPatcher" /v "HideExplorerSearchBar" /t REG_DWORD /d "0" /f
 Reg.exe add "HKCU\Software\ExplorerPatcher" /v "MicaEffectOnTitlebar" /t REG_DWORD /d "0" /f
 Reg.exe add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "Start_ShowClassicMode" /t REG_DWORD /d "0" /f
 Reg.exe add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "TaskbarAl" /t REG_DWORD /d "0" /f
-Reg.exe add "HKCU\Software\ExplorerPatcher" /f
 Reg.exe add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\StartPage" /v "MonitorOverride" /t REG_DWORD /d "0" /f
-Reg.exe add "HKCU\Software\ExplorerPatcher" /f
 Reg.exe add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\StartPage" /v "MakeAllAppsDefault" /t REG_DWORD /d "1" /f
 Reg.exe add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer" /v "AltTabSettings" /t REG_DWORD /d "3" /f
 Reg.exe add "HKCU\Software\ExplorerPatcher" /v "LastSectionInProperties" /t REG_DWORD /d "0" /f
@@ -43,7 +33,6 @@ Reg.exe add "HKCU\Software\ExplorerPatcher" /v "PropertiesInWinX" /t REG_DWORD /
 Reg.exe add "HKCU\Software\ExplorerPatcher" /v "NoMenuAccelerator" /t REG_DWORD /d "1" /f
 Reg.exe add "HKCU\Software\ExplorerPatcher" /v "DisableOfficeHotkeys" /t REG_DWORD /d "1" /f
 Reg.exe add "HKCU\Software\ExplorerPatcher" /v "DisableWinFHotkey" /t REG_DWORD /d "1" /f
-Reg.exe add "HKCU\Software\ExplorerPatcher" /f
 Reg.exe add "HKCU\Software\ExplorerPatcher" /v "DisableAeroSnapQuadrants" /t REG_DWORD /d "0" /f
 Reg.exe add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "Start_PowerButtonAction" /t REG_DWORD /d "2" /f
 Reg.exe add "HKCU\Software\ExplorerPatcher" /v "DoNotRedirectSystemToSettingsApp" /t REG_DWORD /d "0" /f
