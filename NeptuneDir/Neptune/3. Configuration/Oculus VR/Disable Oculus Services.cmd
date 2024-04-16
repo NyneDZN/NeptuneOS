17b0afdc00bd8c23e3ae18dcbf3e1f25cd53f335
NyneDZN <nyneszn@gmail.com>
2024-04-16 14:01:44 -0700

    feat(post-folder) Oculus VR scripts

---
+++ NeptuneDir/Neptune/3. Configuration/Oculus VR/Disable Oculus Services.cmd
@@ -0,0 +1,20 @@
+@echo off
+cd %WinDir%\NeptuneDir\Scripts >nul && where ansi.cmd >nul && call ansi.cmd >nul
+setlocal EnableDelayedExpansion
+
+fltmc >nul 2>&1 || (
+    echo Administrator privileges are required.
+    PowerShell -NoProfile Start -Verb RunAs '%0' 2> nul || (
+        echo Right-click on the script and select 'Run as administrator'.
+        pause & exit 1
+    )
+    exit 0
+)
+
+
+%svcF% QwaveDrv 4
+%svcF% Qwave 4
+%svcF% FontCache 4
+
+echo Disabled Oculus VR. Reboot your device to apply the changes.
+timeout /t 3 /nobreak >nul
