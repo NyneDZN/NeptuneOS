00cc4b39a45419cf645eb7445a98d613c5ce3543
NyneDZN <nyneszn@gmail.com>
2024-04-16 13:58:15 -0700

    feat(post-folder): add event log scripts

---
+++ NeptuneDir/Neptune/4. Advanced Configuration/Event Log & Task Scheduler/Disable Event Log.cmd
@@ -0,0 +1,34 @@
+@echo off
+setlocal EnableDelayedExpansion
+cd %WinDir%\NeptuneDir\Scripts >nul && where ansi.cmd >nul && call ansi.cmd >nul
+
+:: Call Administrator
+fltmc >nul 2>&1 || (
+    echo Administrator privileges are required.
+    PowerShell -NoProfile Start -Verb RunAs '%0' 2> nul || (
+        echo Right-click on the script and select 'Run as administrator'.
+        pause & exit 1
+    )
+    exit 0
+)
+
+echo !S_YELLOW!Disclaimer:
+echo]
+echo !S_RED!Disabling Task Scheduler will break CapFrameX, UWP searching, and a few other things.
+choice /C YN /M "Are you sure you want to continue?"
+if errorlevel 2 (
+    goto Nope
+) else (
+    goto DisableTaskSched
+)
+
+
+
+
+:DisableTaskSched
+%svcF% Eventlog 4
+exit
+
+
+:Nope
+exit
