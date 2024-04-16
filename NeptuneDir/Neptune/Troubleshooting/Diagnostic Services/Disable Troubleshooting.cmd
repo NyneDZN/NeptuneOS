1b7dc8bd946c85794ad3a6ce7fe2e93a52470c5b
NyneDZN <nyneszn@gmail.com>
2024-04-16 14:10:01 -0700

    feat(post-folder): diagnostic service scripts

---
+++ NeptuneDir/Neptune/Troubleshooting/Diagnostic Services/Disable Troubleshooting.cmd
@@ -0,0 +1,16 @@
+@echo off
+cd %WinDir%\NeptuneDir\Scripts >nul && where ansi.cmd >nul && call ansi.cmd >nul
+setlocal EnableDelayedExpansion
+
+whoami /user | find /i "S-1-5-18" > nul 2>&1 || (
+	call RunAsTI.cmd "%~f0" "%*"
+	exit /b 0
+)
+
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\WMI\Autologger\DiagLog" /v "Start" /t REG_DWORD /d "0" /f > nul 2>&1
+%svcF% DPS 4
+%svcF% WdiServiceHost 4
+%svcF% WdiSystemHost 4
+
+echo Disabled Troubleshooting. Restart your device to apply the changes.
+timeout /t 3 /nobreak >nul
