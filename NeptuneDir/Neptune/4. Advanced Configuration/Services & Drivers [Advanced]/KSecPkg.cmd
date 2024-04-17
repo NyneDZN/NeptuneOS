c985bad88f612fe38afc16a0147ee91d7ccac37f
NyneDZN <nyneszn@gmail.com>
2024-04-16 22:11:56 -0700

    feat(post-folder): numerous advanced service/driver config

---
+++ NeptuneDir/Neptune/4. Advanced Configuration/Services & Drivers [Advanced]/KSecPkg.cmd
@@ -0,0 +1,36 @@
+@echo off
+setlocal EnableDelayedExpansion
+cd %WinDir%\NeptuneDir\Scripts >nul && where ansi.cmd >nul && call ansi.cmd >nul
+
+:: Call Administrator
+fltmc >nul 2>&1 || (
+    echo Administrator privileges are required.
+    PowerShell -NoProfile Start -Verb RunAs '%0' 2> nul || (
+        echo Right-click on the script and select 'Run as administrator'.
+        pause & exit 1
+    )
+    exit 0
+)
+echo.  Disable this AFTER you activate windows.
+echo]
+echo   I am unsure what else this may break.
+echo]
+echo]
+echo.  Use this with caution.
+
+
+echo.	Press 1 to Enable
+echo.	Press 2 to Disable
+echo.
+set /p c="Enter your answer: "
+if /i %c% equ 1 goto :enable
+if /i %c% equ 2 goto :disable
+
+
+:disable
+%svcF% KSecPkg 4
+exit
+
+:enable
+%svcF% KSecPkg 0
+exit
