382f3be4c2bc103a920e39662cbf91ebf42bc530
NyneDZN <nyneszn@gmail.com>
2024-04-18 22:36:10 -0700

    feat(ansi.cmd): add winver fetch

--- NeptuneDir/Scripts/ANSI.cmd
+++ NeptuneDir/Scripts/ANSI.cmd
@@ -11,3 +11,12 @@ set neptlog=%WinDir%\NeptuneDir\neptune.txt
 set DevMan="%WinDir%\NeptuneDir\Tools\dmv.exe"
 set svcF=call C:\Windows\NeptuneDir\Scripts\setSvc.cmd
 set delf=del /f /s /q
+
+:: Configure variables for determining winver
+:: - %os% - Windows 10 or 11
+:: - %releaseid% - release ID (21H2, 22H2)
+:: - %build% - current build of Windows (like 10.0.19044.1889)
+for /f "tokens=6 delims=[.] " %%a in ('ver') do (set "win_version=%%a")
+if %win_version% lss 22000 (set os=Windows 10) else (set os=Windows 11)
+for /f "tokens=3" %%a in ('Reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v "DisplayVersion"') do (set releaseid=%%a)
+for /f "tokens=4-7 delims=[.] " %%a in ('ver') do (set "build=%%a.%%b.%%c.%%d")
