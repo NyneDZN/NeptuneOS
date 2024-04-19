d79c17166d6174ae4da81d351d89b900bfc706fb
NyneDZN <nyneszn@gmail.com>
2024-04-18 22:37:19 -0700

    feat(post-folder): default network scripts for troubleshooting

---
+++ NeptuneDir/Neptune/Troubleshooting/Network/Reset Network to Windows Default.cmd
@@ -0,0 +1,27 @@
+@echo off
+cd %WinDir%\NeptuneDir\Scripts >nul && where ansi.cmd >nul && call ansi.cmd >nul
+
+:: Call Administrator
+fltmc >nul 2>&1 || (
+    echo Administrator privileges are required.
+    PowerShell -NoProfile Start -Verb RunAs '%0' 2> nul || (
+        echo Right-click on the script and select 'Run as administrator'.
+        pause & exit 1
+    )
+    exit 0
+)
+
+echo Resetting network settings to Windows defaults...
+
+(
+	netsh int ip reset
+	netsh interface ipv4 reset
+	netsh interface ipv6 reset
+	netsh interface tcp reset
+	netsh winsock reset
+) > nul
+
+
+echo Finished, please reboot your device for changes to apply.
+timeout /t 3 /nobreak >nul
+exit
