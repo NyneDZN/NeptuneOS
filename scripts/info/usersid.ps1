189b93a8ecdeb685c0a80c2f6aa7c6062b71d1ab
Nyne <nyneszn@gmail.com>
2025-09-19 14:46:16 -0700

    dot source to fix variables

--- scripts/info/usersid.ps1
+++ scripts/info/usersid.ps1
@@ -1,13 +1,17 @@
 # usersid.ps1
+
+# Get the current Windows user
 $user = [System.Security.Principal.WindowsIdentity]::GetCurrent()
-$sid  = $user.User.Value
 
-# Terminal output
-Write-Host "Windows SID: $sid"
+# Store the SID as a global variable so it's available in the current session
+$Global:SID = $user.User.Value
 
-# Return structured JSON-ready data
-return @{
+# Display it in the terminal
+Write-Host "Windows SID: $Global:SID"
+
+# Optional: return structured data (useful if capturing output)
+@{
     WindowsUser = @{
-        SID = $sid
+        SID = $Global:SID
     }
 }
--- scripts/modules/execution-helpers.psm1
+++ scripts/modules/execution-helpers.psm1
@@ -18,15 +18,27 @@ function Invoke-ActionScript {
 }
 function Get-SystemInfo {
     param([string]$InfoScript)
+
     $scriptPath = Join-Path $ScriptRoot "scripts\info\$InfoScript"
     if (Test-Path $scriptPath) {
         Write-Log "Gathering info using: $InfoScript"
-        
-        $info = & $scriptPath
-        
+
+        # Dot-source the script so variables persist in this session
+        $info = . $scriptPath
+
         if ($info -is [hashtable]) {
             foreach ($key in $info.Keys) {
+                # Update central system info table
                 Update-SystemInfo -Key $key -Value $info[$key]
+
+                # Also create session-global variables for easy access
+                foreach ($subKey in $info[$key].Keys) {
+                    $varName = $subKey
+                    $varValue = $info[$key][$subKey]
+
+                    # Assign variable in the current session
+                    Set-Variable -Name $varName -Value $varValue -Scope Global
+                }
             }
         } else {
             Write-Log "Info script did not return a hashtable" "ERROR"
