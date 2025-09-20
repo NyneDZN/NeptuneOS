48a491c59b295d719a7174856223d6a406b81146
Nyne <nyneszn@gmail.com>
2025-09-20 15:00:19 -0700

    fix package script quotes

--- scripts/actions/packages.cmd
+++ scripts/actions/packages.cmd
@@ -1,7 +1,7 @@
 :: Disable Defender via Cab
-call "C:\neptune-install\scripts\actions\online-sxs.cmd" "C:\neptune-install\packages\NoDefender.cab
+call "C:\neptune-install\scripts\actions\online-sxs.cmd" "C:\neptune-install\packages\NoDefender.cab"
 
 :: - Silent
 
 :: Disable Telemetry Packages via Cab
-call "C:\neptune-install\scripts\actions\online-sxs.cmd" "C:\neptune-install\packages\NoTelemetry.cab
+call "C:\neptune-install\scripts\actions\online-sxs.cmd" "C:\neptune-install\packages\NoTelemetry.cab"
