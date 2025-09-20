309096ba5f4f6b44a9dffbfa59460268d1815af9
Nyne <nyneszn@gmail.com>
2025-09-20 14:57:05 -0700

    force the reg change

--- scripts/actions/performance/timestamp-interval.cmd
+++ scripts/actions/performance/timestamp-interval.cmd
@@ -1,4 +1,4 @@
 :: This can potentially help smooth things out on a hard drive by setting timestamp logging to background priority, other wise on a sata SSD/NVME, won't do much besides negligably lower background overhead.
 
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Reliability" /v "TimeStampInterval" /t REG_DWORD /d "1" /f
-reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Reliability" /v "IoPriority" /t REG_DWORD /d "3"
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Reliability" /v "IoPriority" /t REG_DWORD /d "3" /f
