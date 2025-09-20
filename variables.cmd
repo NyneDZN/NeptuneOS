48c280e0585856d015e9a458e63fb4c8a80a12d1
Nyne <nyneszn@gmail.com>
2025-09-19 17:32:18 -0700

    attempt to fix nsudo

--- variables.cmd
+++ variables.cmd
@@ -1,9 +1,7 @@
-setlocal enabledelayedexpansion
-
 :: Path variables
 set INITneptdir="C:\neptune-installer"
 set neptdir="C:\Windows\NeptuneDir"
-set currentuser=%INTneptdir%\tools\NSudoLG.exe -U:C -P:E -ShowWindowMode:Hide -Wait
+set currentuser=C:\neptune-installer\tools\NSudoLG.exe -U:C -P:E -ShowWindowMode:Hide -Wait
 
 :: Info scripts
 call "%INITneptidir%\scripts\info\usersid.cmd"
