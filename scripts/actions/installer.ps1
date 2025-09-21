4052c894ac128d729ac2b56fa4ee6813984253e4
Nyne <nyneszn@gmail.com>
2025-09-20 21:43:21 -0700

    run nodefender and notelemetry

--- scripts/actions/installer.ps1
+++ scripts/actions/installer.ps1
@@ -43,8 +43,9 @@ Invoke-ActionScript ".\install-prerequisites.bat"
 Show-Section "Windows Components"
 Invoke-ActionScript ".\components\dism-capabilities.bat"
 Invoke-ActionScript ".\components\binary-removal.cmd"
-Invoke-ActionScript ".\packages.cmd"
+# Invoke-ActionScript ".\packages.cmd"
 Invoke-ActionScript ".\packageInstall.ps1" -InstallPackages "C:\Windows\NeptuneDir\Packages\Z-Atlas-NoDefender-Package31bf3856ad364e35amd645.0.0.0.cab" -NoInteraction
+Invoke-ActionScript ".\packageInstall.ps1" -InstallPackages "C:\Windows\NeptuneDir\Packages\Z-Atlas-NoTelemetry-Package31bf3856ad364e35amd645.0.0.0.cab" -NoInteraction
 Invoke-ActionScript ".\CLIENTCBS.ps1"
 
 # Chocolatey
