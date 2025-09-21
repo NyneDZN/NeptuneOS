3ed18076713735eccf1fb587af650788db4aeee0
Nyne <nyneszn@gmail.com>
2025-09-20 20:30:22 -0700

    update defender toggle

--- os/NeptuneDir/Neptune/6. Security/Windows Defender/Toggle Defender.cmd
+++ os/NeptuneDir/Neptune/6. Security/Windows Defender/Toggle Defender.cmd
@@ -1,6 +1,5 @@
 @echo off
-cd %WinDir%\NeptuneDir\Scripts >nul && where ansi.cmd >nul && call ansi.cmd >nul
-set "script=%windir%\NeptuneDir\Scripts\ToggleDefender.ps1"
+set "script=%windir%\NeptuneDir\Scripts\ScriptWrappers\ToggleDefender.ps1"
 if not exist "%script%" (
 	echo Script not found.
 	echo "%script%"
@@ -8,15 +7,9 @@ if not exist "%script%" (
 	exit /b 1
 )
 
-set "___args="%~f0" %*"
-fltmc > nul 2>&1 || (
-	echo Administrator privileges are required.
-	powershell -c "Start-Process -Verb RunAs -FilePath 'cmd' -ArgumentList """/c $env:___args"""" 2> nul || (
-		echo You must run this script as admin.
-		if "%*"=="" pause
-		exit /b 1
-	)
+whoami /user | find /i "S-1-5-18" > nul 2>&1 || (
+	call C:\Windows\NeptuneDir\Scripts\RunAsTI.cmd "%~f0" %*
 	exit /b
 )
-echo Opened the Toggle Defender Script >> %userlog%
+
 powershell -EP Bypass -NoP ^& """$env:script""" %*
