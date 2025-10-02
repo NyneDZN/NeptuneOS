31b194302fe8ca8fd93e95d3b2067e06263b5920
Nyne <nyneszn@gmail.com>
2025-10-02 15:08:37 -0700

    remove if variable for congestionprovider

--- scripts/actions/tweaks/network/netsh.cmd
+++ scripts/actions/tweaks/network/netsh.cmd
@@ -42,13 +42,11 @@ netsh int tcp set global chimney=disabled
 :: https://www.speedguide.net/articles/tcp-congestion-control-algorithms-comparison-7423
 netsh int tcp set global congestionprovider=ctcp 
 :: - > Set Congestion Provider to BBR2 on Windows 11
-if "%os%"=="Windows 11" (
 netsh int tcp set supplemental Template=Internet CongestionProvider=bbr2 
 netsh int tcp set supplemental Template=Datacenter CongestionProvider=bbr2 
 netsh int tcp set supplemental Template=Compat CongestionProvider=bbr2 
 netsh int tcp set supplemental Template=DatacenterCustom CongestionProvider=bbr2 
 netsh int tcp set supplemental Template=InternetCustom CongestionProvider=bbr2 
-)
 :: - > Enable Direct Cache Access
 :: - > This will have a bigger impact on older CPU's
 netsh int tcp set global dca=enabled 
