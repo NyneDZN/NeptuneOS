becc3e7537402656290fbe96d84ab12a09359784
Nyne <nyneszn@gmail.com>
2025-10-02 14:25:12 -0700

    fix toggle defender

--- os/NeptuneDir/Scripts/ScriptWrappers/ToggleDefender.ps1
+++ os/NeptuneDir/Scripts/ScriptWrappers/ToggleDefender.ps1
@@ -21,7 +21,7 @@ if (!(Test-Path $packageInstall)) {
     exit 1
 }
 
-$package = "NoDefender*"
+$package = "*Z-Atlas-NoDefender-Package*"
 
 try {
 	$packages = (Get-WindowsPackage -online | Where-Object { $_.PackageName -like "*NoDefender*" }).PackageName
