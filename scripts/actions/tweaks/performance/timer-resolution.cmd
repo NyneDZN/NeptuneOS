36d9bdf9f18a5d07e22931925a0570aab002b505
Nyne <nyneszn@gmail.com>
2025-10-02 19:46:16 -0700

    rename timer res task

--- scripts/actions/tweaks/performance/timer-resolution.cmd
+++ scripts/actions/tweaks/performance/timer-resolution.cmd
@@ -1,3 +1,3 @@
 :: Registers the timer resolution task from AtlasOS
 :: Set to 0.6ms
-schtasks /create /tn "MyTaskName" /xml "%WinDir%\NeptuneDir\Tasks\Force Timer Resolution.xml"
+schtasks /create /tn "Timer Resolution" /xml "%WinDir%\NeptuneDir\Tasks\Force Timer Resolution.xml"
