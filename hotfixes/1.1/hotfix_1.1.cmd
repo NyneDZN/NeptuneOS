e2fcc69b46bbc44dcc7a541562fb9d32c145ca69
Nyne <nyneszn@gmail.com>
2025-10-12 14:47:29 -0700

    feat: BBR2 congestion provider and fix bullets, final commit

--- hotfixes/1.1/hotfix_1.1.cmd
+++ hotfixes/1.1/hotfix_1.1.cmd
@@ -36,6 +36,9 @@ call "C:\Windows\NeptuneDir\Updates\updater_variables.cmd"
 :: Fullscreen script
 "%WinDir%\NeptuneDir\Updates\hotfixes\1.1\FullscreenCMD.vbs" >nul
 
+:: Change code page for bullet points
+chcp 65001>nul
+
 
 :: Echo to user
 cls & echo !S_GREEN!NeptuneOS V1.1!S_GREEN! !S_WHITE!Hotfix!S_WHITE!
@@ -62,6 +65,9 @@ reg add "HKEY_LOCAL_MACHINE\SOFTWARE\NeptuneOS" /v NeptuneVersion /t REG_SZ /d "
 :: ----------------------------------------------------------
 ::                  Begin Hotfixes
 :: ----------------------------------------------------------
+
+:: Change code page back before doing things
+chcp 437>nul
 
 :: Enable notification services to fix calendar and explorer crashes
 echo !S_WHITE!Enabling notification services!S_WHITE!
@@ -95,6 +101,14 @@ reg add "HKCU\Control Panel\Mouse" /v "RawMouseThrottleDuration" /t REG_DWORD /d
 :: Disable platform tick
 echo !S_WHITE!Disabling platform tick!S_WHITE!
 bcdedit /deletevalue useplatformtick >nul
+
+:: Setting BBR2 for TCP Congestion Provider
+echo !S_WHITE!Setting BBR2 for TCP Congestion Provider!S_WHITE!
+netsh int tcp set supplemental Template=Internet CongestionProvider=bbr2 
+netsh int tcp set supplemental Template=Datacenter CongestionProvider=bbr2 
+netsh int tcp set supplemental Template=Compat CongestionProvider=bbr2 
+netsh int tcp set supplemental Template=DatacenterCustom CongestionProvider=bbr2 
+netsh int tcp set supplemental Template=InternetCustom CongestionProvider=bbr
 
 
 :: TImeout to let user see what was done
