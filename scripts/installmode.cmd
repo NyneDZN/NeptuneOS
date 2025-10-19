7b4019c8fd37a053caf82b5d0ebc7c30d13b2196
Nyne <nyneszn@gmail.com>
2025-10-19 14:39:47 -0700

    feat: stop pausing on first open

--- scripts/installmode.cmd
+++ scripts/installmode.cmd
@@ -1,16 +1,10 @@
 @echo off
-setlocal
-
 :: Go up one folder from where this script is located
 set "PARENT=%~dp0.."
 for %%I in ("%PARENT%") do set "ROOT=%%~fI"
 
 :: Debug - show the path being used
 echo Running from: %ROOT%\master.ps1
-pause
 
 :: Run the script without elevation for now
 "%ROOT%\tools\MinSudo.exe" -NoL -P -V -WD=%ROOT% C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%ROOT%\master.ps1" -NeptuneInstall
-
-endlocal
-pause
