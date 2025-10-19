f27b6843e4739ab78e9f66a9ead98aaf5ab86fb2
Nyne <nyneszn@gmail.com>
2025-10-19 14:43:24 -0700

    feat: stop clearing event logs

--- scripts/actions/installer.ps1
+++ scripts/actions/installer.ps1
@@ -12,7 +12,7 @@ Clear-Host
 Invoke-ActionScript "ngen.ps1"
 
 # Clear event logs
-Invoke-ActionScript ".\tweaks\EVENTLOGS.cmd"
+# Invoke-ActionScript ".\tweaks\EVENTLOGS.cmd"
 
 # Initialize Neptune Environment
 Move-Item -Path ".\os\NeptuneDir" -Destination "$env:WINDIR" -Force
