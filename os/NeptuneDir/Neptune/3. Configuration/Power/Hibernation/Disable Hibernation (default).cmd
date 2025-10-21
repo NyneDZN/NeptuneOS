bc9ac1777e0c0e2d05e1dd6f70bcb33de91838df
Nyne <nyneszn@gmail.com>
2025-10-20 22:34:24 -0700

    mass: neptune folder revision

---
+++ os/NeptuneDir/Neptune/3. Configuration/Power/Hibernation/Disable Hibernation (default).cmd
@@ -0,0 +1,23 @@
+@echo off
+
+if "%~1" == "/silent" goto main
+
+set "___args="%~f0" %*"
+fltmc > nul 2>&1 || (
+	echo Administrator privileges are required.
+	powershell -c "Start-Process -Verb RunAs -FilePath 'cmd' -ArgumentList """/c $env:___args"""" 2> nul || (
+		echo You must run this script as admin.
+		if "%*"=="" pause
+		exit /b 1
+	)
+	exit /b
+)
+
+:main
+powercfg /h off
+
+if "%~1" == "/silent" exit
+
+echo Finished, changes have been applied.
+pause
+exit /b
