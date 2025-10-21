20a09b4010743a8f1aafd84bac88d425b90df20b
Nyne <nyneszn@gmail.com>
2025-10-20 19:12:59 -0700

    fix: move timer res config to 3. configuration

---
+++ os/NeptuneDir/Neptune/3. Configuration/Timer Resolution/Disable Timer Resolution.cmd
@@ -0,0 +1,20 @@
+@echo off
+
+set "___args="%~f0" %*"
+fltmc > nul 2>&1 || (
+	echo Administrator privileges are required.
+	powershell -c "Start-Process -Verb RunAs -FilePath 'cmd' -ArgumentList """/c $env:___args"""" 2> nul || (
+		echo You must run this script as admin.
+		if "%*"=="" pause
+		exit /b 1
+	)
+	exit /b
+)
+
+reg delete "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\kernel" /v "GlobalTimerResolutionRequests" /f > nul 2>&1
+taskkill /f /im SetTimerResolution.exe > nul 2>&1
+schtasks /delete /tn "Force Timer Resolution" /f > nul 2>&1
+
+echo Finished, changes have been applied.
+pause
+exit /b
---
+++ os/NeptuneDir/Neptune/3. Configuration/Timer Resolution/Enable Timer Resolution.cmd
@@ -0,0 +1,24 @@
+@echo off
+
+set "___args="%~f0" %*"
+fltmc > nul 2>&1 || (
+	echo Administrator privileges are required.
+	powershell -c "Start-Process -Verb RunAs -FilePath 'cmd' -ArgumentList """/c $env:___args"""" 2> nul || (
+		echo You must run this script as admin.
+		if "%*"=="" pause
+		exit /b 1
+	)
+	exit /b
+)
+
+echo Before running this, please see the Atlas documentation, linked in the folder.
+pause
+echo]
+
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\kernel" /v "GlobalTimerResolutionRequests" /t REG_DWORD /d "1" /f > nul
+schtasks /create /tn "Force Timer Resolution" /xml "%windir%\NeptuneDir\Tasks\Force Timer Resolution.xml" /f > nul
+schtasks /run /tn "Force Timer Resolution" > nul
+
+echo Finished, please reboot your device for changes to apply.
+pause
+exit /b
