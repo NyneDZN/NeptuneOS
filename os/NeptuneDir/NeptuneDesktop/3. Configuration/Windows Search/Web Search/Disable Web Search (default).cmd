f74ac0adbf89c34dec0421e492b2c463818c99b5
Nyne <nyneszn@gmail.com>
2025-10-31 00:52:40 -0700

    revise: web search configuration scripts

--- os/NeptuneDir/NeptuneDesktop/3. Configuration/Windows Search/Web Search/Disable Web Search (default).cmd
+++ os/NeptuneDir/NeptuneDesktop/3. Configuration/Windows Search/Web Search/Disable Web Search (default).cmd
@@ -1,19 +1,22 @@
 @echo off
+title Disable Web Search
 
+:: Init
+call "%WinDir%\NeptuneDir\Scripts\envINIT.cmd"
+
+:: Run as administrator
 set "___args="%~f0" %*"
 fltmc > nul 2>&1 || (
-	echo Administrator privileges are required.
-	powershell -c "Start-Process -Verb RunAs -FilePath 'cmd' -ArgumentList """/c $env:___args"""" 2> nul || (
-		echo You must run this script as admin.
-		if "%*"=="" pause
-		exit /b 1
-	)
-	exit /b
+    echo Administrator privileges are required.
+    powershell -c "Start-Process -Verb RunAs -FilePath 'cmd' -ArgumentList """/c $env:___args"""" 2> nul || (
+        echo You must run this script as admin.
+        if "%*"=="" pause
+        exit /b 1
+    )
+    exit /b
 )
 
-echo]
-echo Disabling Web Search ^& Search Highlights...
-
+:: Disable Web Search & Search Highlights
 call "%windir%\NeptuneDir\Scripts\settingsPages.cmd" /hide search-permissions /silent
 
 (
@@ -34,10 +37,13 @@ call "%windir%\NeptuneDir\Scripts\settingsPages.cmd" /hide search-permissions /s
     start explorer.exe
 ) > nul 2>&1
 
+:: Exit script if called with /silent argument
+if "%~1"=="/silent" exit /b
+
+:: Echo back to user
 echo]
-:: Echo to Log
-echo %date% %time% Disabled Web Search >> %userlog%
-:: Echo to User
-echo !S_YELLOW!Web Search has been disabled.
-timeout /t 3 /nobreak >nul
-exit
+echo %S_GREEN%Disabled Web Search and Search Highlights%S_GREEN%
+echo]
+echo %S_WHITE%Press any key to exit...%S_WHITE%
+pause > nul
+exit /b
--- os/NeptuneDir/NeptuneDesktop/3. Configuration/Windows Search/Web Search/Enable Web Search.cmd
+++ os/NeptuneDir/NeptuneDesktop/3. Configuration/Windows Search/Web Search/Enable Web Search.cmd
@@ -1,19 +1,22 @@
 @echo off
+title Configuration
 
+:: Init
+call "%WinDir%\NeptuneDir\Scripts\envINIT.cmd"
+
+:: Run as administrator
 set "___args="%~f0" %*"
 fltmc > nul 2>&1 || (
-	echo Administrator privileges are required.
-	powershell -c "Start-Process -Verb RunAs -FilePath 'cmd' -ArgumentList """/c $env:___args"""" 2> nul || (
-		echo You must run this script as admin.
-		if "%*"=="" pause
-		exit /b 1
-	)
-	exit /b
+    echo Administrator privileges are required.
+    powershell -c "Start-Process -Verb RunAs -FilePath 'cmd' -ArgumentList """/c $env:___args"""" 2> nul || (
+        echo You must run this script as admin.
+        if "%*"=="" pause
+        exit /b 1
+    )
+    exit /b
 )
 
-echo]
-echo Enabling Web Search ^& Search Highlights...
-
+:: Enable Web Search & Search Highlights
 call "%windir%\NeptuneDir\Scripts\settingsPages.cmd" /unhide search-permissions /silent
 
 (
@@ -35,11 +38,13 @@ call "%windir%\NeptuneDir\Scripts\settingsPages.cmd" /unhide search-permissions 
 ) > nul 2>&1
 
 
+:: Exit script if called with /silent argument
+if "%~1"=="/silent" exit /b
+
+:: Echo back to user
 echo]
+echo %S_GREEN%Enabled Web Search and Search Highlights%S_GREEN%
 echo]
-:: Echo to Log
-echo %date% %time% Enabled Web Search >> %userlog%
-:: Echo to User
-echo !S_YELLOW!Web Search has been enabled.
-timeout /t 3 /nobreak >nul
-exit
+echo %S_WHITE%Press any key to exit...%S_WHITE%
+pause > nul
+exit /b
