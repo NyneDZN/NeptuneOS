10883c3f6aea08318d0bcf5019ca64bd939f3ab7
Nyne <nyneszn@gmail.com>
2025-10-30 19:59:11 -0700

    remove: systemsettingsextensions.dll

--- scripts/actions/components/binary-removal.cmd
+++ scripts/actions/components/binary-removal.cmd
@@ -12,4 +12,11 @@ taskkill /f /im mobsync.exe
 cd %WinDir%\System32 
 takeown /f "mobsync.exe" 
 icacls "%WinDir%\System32\mobsync.exe" /grant Administrators:F 
-ren mobsync.exe mobsync.old 
+ren mobsync.exe mobsync.old
+
+:: SystemSettingExtensions
+takeown /f "C:\Windows\SystemApps\MicrosoftWindows.Client.Core_cw5n1h2txyewy\SystemSettingsExtensions.dll" /a
+cd "C:\Windows\SystemApps\MicrosoftWindows.Client.Core_cw5n1h2txyewy\"
+ren "SystemSettingsExtensions.dll" "SystemSettingsExtensions.old"
+
+
