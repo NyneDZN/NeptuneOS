0345cea08e29765c403130ecbf824d210edbe603
Nyne <nyneszn@gmail.com>
2025-10-31 00:52:57 -0700

    revise: sleep states configuration scripts

--- os/NeptuneDir/NeptuneDesktop/3. Configuration/Power/Sleep States/Enable Sleep States.bat
+++ os/NeptuneDir/NeptuneDesktop/3. Configuration/Power/Sleep States/Enable Sleep States.bat
@@ -1,3 +1,22 @@
+@echo off
+title Sleep States
+
+:: Init
+call "%WinDir%\NeptuneDir\Scripts\envINIT.cmd"
+
+:: Run as administrator
+set "___args="%~f0" %*"
+fltmc > nul 2>&1 || (
+    echo Administrator privileges are required.
+    powershell -c "Start-Process -Verb RunAs -FilePath 'cmd' -ArgumentList """/c $env:___args"""" 2> nul || (
+        echo You must run this script as admin.
+        if "%*"=="" pause
+        exit /b 1
+    )
+    exit /b
+)
+
+:: Enable sleep states
 powercfg -setacvalueindex 11111111-1111-1111-1111-111111111111 238c9fa8-0aad-41ed-83f4-97be242c8f20 25dfa149-5dd1-4736-b5ab-e8a37b5b8187 1
 powercfg -setdcvalueindex 11111111-1111-1111-1111-111111111111 238c9fa8-0aad-41ed-83f4-97be242c8f20 25dfa149-5dd1-4736-b5ab-e8a37b5b8187 1
 powercfg -setacvalueindex 11111111-1111-1111-1111-111111111111 238c9fa8-0aad-41ed-83f4-97be242c8f20 abfc2519-3608-4c2a-94ea-171b0ed546ab 1
@@ -6,9 +25,10 @@ powercfg -setacvalueindex 11111111-1111-1111-1111-111111111111 238c9fa8-0aad-41e
 powercfg -setdcvalueindex 11111111-1111-1111-1111-111111111111 238c9fa8-0aad-41ed-83f4-97be242c8f20 94ac6d29-73ce-41a6-809f-6363ba21b47e 1
 powercfg -setactive scheme_current
 
-:: Echo to Log
-echo %date% %time% Enabled Sleep States >> %userlog%
-:: Echo to User
-echo !S_YELLOW!Sleep States have been enabled
-timeout /t 3 /nobreak >nul
-exit
+:: Echo back to user
+echo]
+echo %S_GREEN%Enabled Sleep States%S_GREEN%
+echo]
+echo %S_WHITE%Press any key to exit...%S_WHITE%
+pause > nul
+exit /b
